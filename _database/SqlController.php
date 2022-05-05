7d3346d81dc118afd441842fc0c71c723a871c5d
Rodrigo-369 <Rodricx_22@hotmail.com>
2022-05-04 23:15:20 -0300

    Classe de acesso ao banco

---
+++ _database/SqlController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace SQL;
+
+use PDO;
+
+Abstract class Sql extends PDO{
+
+    protected $conn;
+
+    public function __construct()
+    {
+        $this->conn = new PDO("mysql:host=localhost;dbname=db_academy", "root", "");
+    }
+
+}
+
+$n = new Sql();
+
+?>
